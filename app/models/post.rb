f194d64cb2281dc653b4bc1dbcd2ccf6d2baf231
Olanrewaju Oduola <111298026+David-Lanzz@users.noreply.github.com>
2023-06-16 23:49:51 +0100

    Add correct relationship to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,9 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :comments
+  has_many :likes
+
+  after_save :update_post_counter
 
   def update_post_counter
     author.update(posts_counter: author.posts.count)
