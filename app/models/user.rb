d4ba146128f14bc844d429355b840810bd0993cc
Olanrewaju Oduola <111298026+David-Lanzz@users.noreply.github.com>
2023-06-17 21:35:57 +0100

    Complete tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,9 @@ class User < ApplicationRecord
   has_many :comments
   has_many :likes
 
-  validates :name,presence:true
+  validates :name, presence: true
   validates :posts_counter,
-  numericality: { only_integer: true,greater_than_or_equal_to:0}
+            numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def recent_posts
     posts.order(created_at: :desc).limit(3)
