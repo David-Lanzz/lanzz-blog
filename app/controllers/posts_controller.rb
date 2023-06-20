71af1d2c04a8ef2958a46510becba54abbff300d
Olanrewaju Oduola <111298026+David-Lanzz@users.noreply.github.com>
2023-06-20 17:11:36 +0100

    Update controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,37 +1,7 @@
 class PostsController < ApplicationController
   def index
-    @post = Post.all
   end
 
   def show
-    @post = Post.find(params[:id])
-  end
-
-  def new
-    @post = Post.new
-  end
-
-  def create
-    @post = Post.new(posts_params)
-
-    if @post.save
-      redirect_to @post
-    else
-      render 'new'
-    end
-  end
-
-  def update
-    @post = Post.find(params[:id])
-
-    if @post.update(posts_params)
-      redirect_to @post
-    else
-      render 'new'
-    end
-  end
-
-  def posts_params
-    params.require(:post).permit(:author_id, :title, :text)
   end
 end
