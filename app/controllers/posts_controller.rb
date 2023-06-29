2e38284b87914d219ad68e4bfcf4a1fcd159f5f3
Olanrewaju Oduola <111298026+David-Lanzz@users.noreply.github.com>
2023-06-29 08:22:53 +0100

    Finish tests

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @posts = @user.posts.includes(:author)
+    @posts = @user.posts.includes(:comments)
     @users = User.includes(:posts)
   end
 
